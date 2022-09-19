6f1657173567740b6e2be66b896d06b6a9aa1c5c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-09-19 10:56:34 -0500

    Create sript.js

---
+++ sript.js
@@ -0,0 +1,4 @@
+var userName = document.getElementById("theirName");
+function press(){
+  document.getElementById("answer").innerHTML = "hello " +  userName.value + " Have a look at this stuff."
+}
